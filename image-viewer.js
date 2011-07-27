8cd7f16c7ff32e50d7eb5616cdd4b9e71bde582e
Ben Wells <benfred@mjaw.com>
2011-07-27 22:46:11 +1000

    Working: Current pixel tracking (Chrome).

--- image-viewer.js
+++ image-viewer.js
@@ -1,6 +1,6 @@
 // When I know how these should go into some sort of custom object
 gvWidth = 50;
-gvHeight = 20;
+gvHeight = 50;
 gvZoom = 10;
 // How to get these from CSS?
 BACKGROUND_COLOR = 'rgb(255,255,255)';
@@ -11,51 +11,71 @@ MIN_ZOOM_FOR_GRID = 4;
 BORDER_WIDTH = 2;
 GRID_WIDTH = 1;
 
-function vpWidth() {
+function canvasWidth() {
   return (gvWidth * gvZoom) + 1;
 }
 
-function vpHeight() {
+function canvasHeight() {
   return (gvHeight * gvZoom);
 }
 
 function sizeCanvas(canvas) {
-  canvas.width = vpWidth();
-  canvas.height = vpHeight();
+  canvas.width = canvasWidth();
+  canvas.height = canvasHeight();
 }
 
 function drawBorder(context) {
   context.fillStyle = BACKGROUND_COLOR;
-  context.fillRect(0, 0, vpWidth(), vpHeight());
+  context.fillRect(0, 0, canvasWidth(), canvasHeight());
   context.lineWidth = 1;
   context.strokeStyle = BORDER_COLOR;
-  context.strokeRect(0.5, 0.5, vpWidth() - 1, vpHeight() - 1);
+  context.strokeRect(0.5, 0.5, canvasWidth() - 1, canvasHeight() - 1);
 }
 
 function drawGrid(context) {
   // Don't draw the grid if it would obscure the drawing
   if (gvZoom <= MIN_ZOOM_FOR_GRID) return;
-  // don't draw first horizontal or vertical gridline as it is already
+  // Don't draw first horizontal or vertical gridline as it is already
   // done by the border.
+  // Need a drawLine function here...
   context.lineWidth = 1;
   context.strokeStyle = GRID_COLOR;
   for (var x = 1; x < gvWidth; ++x) {
     context.beginPath();
     context.moveTo(x * gvZoom + 0.5, 1);
-    context.lineTo(x * gvZoom + 0.5, vpHeight() - 1);
+    context.lineTo(x * gvZoom + 0.5, canvasHeight() - 1);
     context.stroke();
   }
   for (var y = 1; y < gvHeight; ++y) {
     context.beginPath();
     context.moveTo(1, y * gvZoom + 0.5);
-    context.lineTo(vpWidth() - 1, y * gvZoom + 0.5);
+    context.lineTo(canvasWidth() - 1, y * gvZoom + 0.5);
     context.stroke();
+  }
+}
+
+// Turn this into a closure so no need to keep getting elements.
+function handleCanvasMouseMove(event) {
+  var canvas = document.getElementById('imagecanvas');
+  var xPosEl = document.getElementById('xpos');
+  var yPosEl = document.getElementById('ypos');
+  // Does not work in firefox when window is scrolled :(
+  var worldX = event.clientX - canvas.offsetLeft - BORDER_WIDTH + document.body.scrollLeft;
+  worldX = Math.floor(worldX / gvZoom);
+  if (worldX >= 0 && worldX < gvWidth) {
+    xPosEl.innerHTML = worldX;
+  }
+  var worldY = event.clientY - canvas.offsetTop - BORDER_WIDTH + document.body.scrollTop;
+  worldY = Math.floor(worldY / gvZoom);
+  if (worldY >= 0 && worldY < gvHeight) {
+    yPosEl.innerHTML = worldY;
   }
 }
 
 function initView() {
   var canvas = document.getElementById('imagecanvas');
   if (canvas && canvas.getContext) {
+    canvas.onmousemove = handleCanvasMouseMove;
     var context = canvas.getContext('2d');
     context.globalAlpha = 1.0;
     sizeCanvas(canvas);
